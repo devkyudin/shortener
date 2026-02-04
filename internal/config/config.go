ed8c1019510a49b6125e4016c6258382613ed6a0
Konstantin Yudin <dev.kyudin@gmail.com>
2026-02-04 22:08:59 +0200

    remove trash

--- internal/config/config.go
+++ internal/config/config.go
@@ -14,7 +14,7 @@ type Config struct {
 }
 
 type NetAddress struct {
-	Protocol string `json:"protocol"  bson:"protocol" json:"brotocol, omitempty"`
+	Protocol string
 	Host     string
 	Port     string
 }
